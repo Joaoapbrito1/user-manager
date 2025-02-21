2995d22ef66d53c61b7891febe0a2214969f5f44
Joaoapbrito1 <joao.brito@zup.com.br>
2025-02-21 16:57:08 -0300

    feat: implements the UserService interface.

---
+++ src/main/java/br/com/user_manager/service/UserService.java
@@ -0,0 +1,21 @@
+package br.com.user_manager.service;
+
+import br.com.user_manager.repository.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    //busca um usuário no banco de dados pelo login (username) usando o UserRepository e retorna um objeto UserDetails para autenticação no Spring Security.
+    @Autowired
+    UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByLogin(username);
+    }
+}
